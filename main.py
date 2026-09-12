b2938f723af613a7195dd4f1bd00735693b6532b
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-12 23:42:00 +0300

    Implement Test FastAPI application with welcome endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from minirag-26!")
+from fastapi import FastAPI
+
+app = FastAPI(title="MiniRag_26", version="0.1", description="Question Answering System")
+
+@app.get("/welcome")
+def welcome():
+    return {"message": "Welcome to MiniRag_26!"}
 
 
-if __name__ == "__main__":
-    main()
