cf8123550ed14c2eaa73636ab2e40fbe21110805
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-13 22:49:02 +0300

    REFCTOR update read_root to use Settings type hint for app_settings

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends
-from helpers import get_settings
+from helpers import Settings, get_settings
 
 
 base_router = APIRouter(prefix="/api/v1",
@@ -7,7 +7,7 @@ base_router = APIRouter(prefix="/api/v1",
                         )
 
 @base_router.get("/")
-async def read_root(app_settings=Depends(get_settings)):
+async def read_root(app_settings: Settings=Depends(get_settings)):
 
     return {'app_name': app_settings.APP_NAME, 
             'version': app_settings.APP_VERSION, 
