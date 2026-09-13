e1c0b5b35bffc575af250bbf21e4d0822b10a16a
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-13 22:06:28 +0300

    CREATE config module with Pydantic settings to load .env file and validate it
    CERATE get_settings to return object of app settings

---
+++ src/helpers/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    APP_NAME: str
+    APP_VERSION: str
+    APP_DESCRIPTION: str
+    
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
+        
+def get_settings() -> Settings:
+    return Settings()
