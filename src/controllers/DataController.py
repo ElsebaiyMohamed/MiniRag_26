d6a69ccb13e726364c257132e0b5bc577da5cf7c
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-14 17:17:34 +0300

    ADD generate unique filename and clean filename method in DataController; update upload endpoint to use cleaned filename insted of orignal filename

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import UploadFile
 from .BaseController import BaseController
 from models import ResponseSignal
@@ -19,3 +21,12 @@ class DataController(BaseController):
         
         return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
         
+    def gen_uniqe_filename(self, org_filename, project_path):
+        
+        rand_filename = self._gen_rand_str()
+        clean_filename = self.__cleaned_filemame(org_filename)
+        
+        return rand_filename + '_' + clean_filename
+    
+    def __cleaned_filemame(self, filename: str):
+        return re.sub(r'[^\w.]', '', filename.strip()).replace(' ', '_')
