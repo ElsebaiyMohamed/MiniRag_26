faa5dbe7c8a9b693b277372a50f0b35231f58bb7
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-14 16:29:04 +0300

    ADD recive uploaded file as stream of chunks and save it locally on assets

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,12 @@
+import os
+import aiofiles
+
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 
 from helpers import Settings, get_settings
 from controllers import DataController, ProjectController
+from models import ResponseSignal
 
 data_router = APIRouter(prefix="/api/v1/data",
                         tags=["v1", "data"]
@@ -22,3 +26,14 @@ async def upload(project_id: str, file: UploadFile, app_settings: Settings=Depen
                             )
     
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = os.path.join(project_dir_path, file.filename)
+    async with aiofiles.open(file_path, 'wb') as f:
+        while chunk := await file.read(app_settings.FILE_DEAFAULT_CHINK_SIZE):
+            await f.write(chunk)
+            
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={
+            'signal': ResponseSignal.FILE_UPLOAD_SUCCESS.value
+        } 
+    )
