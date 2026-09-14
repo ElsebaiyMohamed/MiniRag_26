9143a2f33093608008b1e858cad1af7f0be17c18
ElsebaiyMohamed <Sebaeymohamed43@gmail.com>
2026-09-14 16:12:58 +0300

    REMOVE commented-out code for successful upload response in data upload endpoint

--- src/routes/data.py
+++ src/routes/data.py
@@ -22,14 +22,3 @@ async def upload(project_id: str, file: UploadFile, app_settings: Settings=Depen
                             )
     
     project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    
-    
-    
-    
-    # if is_valid:
-    #     return JSONResponse(status_code=status.HTTP_200_OK, 
-    #                         content={
-    #                                 "signal": signal
-    #                             }
-                                
-    #                             )
