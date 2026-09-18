cdd398dfa4baef993b45a6500bd644209cfc03f5
ElsebaiyMohamed <sebaiy.mohamed.edu@gmail.com>
2026-09-18 05:14:21 +0300

    FIX: correct typo in database client assignment in startup event

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ async def start_db_client():
     settings = get_settings()
     
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
-    app.dp_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
 @app.on_event('shutdown')
 
