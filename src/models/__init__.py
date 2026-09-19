b3ae7946ae79476a0e4c0cadb76b3725cd5cbf5d
ElsebaiyMohamed <sebaiy.mohamed.edu@gmail.com>
2026-09-19 04:46:41 +0300

    ADD: implement ChunkDataModel with CRUD operations for managing data chunks

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from .enums.ResponseEnums import ResponseSignal
 from .enums.ProcessingEnums import ProcessingEnum
 from .schemas.data import ProcessRequest
 from .ProjectDataModel import ProjectDataModel
+from .ChunkDataModel import ChunkDataModel
